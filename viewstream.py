4264dc01808fd34d621411bd86c49e7e7897858f
Brad Roodt <brad.roodt@gmail.com>
2020-08-29 20:35:30 +0200

    add config.json

--- viewstream.py
+++ viewstream.py
@@ -1,25 +1,27 @@
 import cv2
 import threading
-connection_retry_interval = 5
-urls = ["http://192.168.8.107:8080/stream/video.mjpeg"]
+import json
+
+with open('config.json') as file:
+  config = json.load(file)
+
+urls = config['urls']
+object_detection_interval = config['object_detection_interval']
+stream_connect_retry_interval= config['stream_connect_retry_interval']
+show_preview = config['show_preview']
 streams = []
 fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
 out = cv2.VideoWriter('output.mp4',fourcc, 10, (640,480))
 frameCount=0
 
-for url in urls:
+for url in config['urls']:
   streams.append({'cap' : None, 'url' : url})
 
-def setStream(i):
-  print(cv2)
-  #globals()['stream' + str(i)] = cv2.videoCapture(url)
-  active_stream = streams[i]
-  print(active_stream['cap'])
-  print(active_stream['url'])
+def setStream(i):  
+  active_stream = streams[i]  
   active_stream ['cap'] = cv2.VideoCapture(active_stream['url'])
 
-def isStreamOpen(i):
-  #return globals()['stream' + str(i)].isOpened() 
+def isStreamOpen(i):  
   if (streams[i]['cap'] == None):
      return False 
   return streams[i]['cap'].isOpened()
@@ -28,8 +30,8 @@ def connect(i):
     print('connect')      
     setStream(i)
     if not isStreamOpen(i):                     
-      print("Unable to read stream. Trying again in 5 seconds")      
-      t=threading.Timer(connection_retry_interval,connect, [i])
+      print("Unable to read stream from camera at url: {}. Trying again in 5 seconds".format(streams[i]['url']))      
+      t=threading.Timer(stream_connect_retry_interval,connect, [i])
       t.start()        
         
 connect(0)
@@ -44,7 +46,7 @@ while True:
       frameCount = 0      
       #out.write(frame)       
     frameCount+=1    
-    cv2.imshow('Video', frame)     
+    if show_preview: cv2.imshow(streams[0]['url'], frame)     
     if cv2.waitKey(1) & 0xFF == ord('q'):
           break
   else:
@@ -52,7 +54,7 @@ while True:
     active_stream.release()
     out.release()
     cv2.destroyAllWindows()
-    t=threading.Timer(5,connect,[0])
+    t=threading.Timer(stream_connect_retry_interval, connect,[0])
     t.start()
 print('cap broken')    
 active_stream.release()
