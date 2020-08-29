27841c0cdde64594747ceb2a8d0373048c079359
Brad Roodt <brad.roodt@gmail.com>
2020-08-29 18:54:46 +0200

    Reconnects working

--- viewstream.py
+++ viewstream.py
@@ -1,26 +1,56 @@
 import cv2
-cap = cv2.VideoCapture("http://192.168.8.107:8080/stream/video.mjpeg")
-print(type(cap))
+import threading
+stream1 = None
+isConnected = False
+def connect():  
+    print('connect')  
+    global stream1 
+    global isConnected
+    stream1 = cv2.VideoCapture("http://192.168.8.107:8080/stream/video.mjpeg")
+    if stream1.isOpened():
+      print('setting is Connected')
+      isConnected = True
+    else:
+        isConnected = False
+        t=threading.Timer(5,connect)
+        t.start()
+        print('no read streamy. try againy')
+        #raise NameError('Just a Dummy Exception, write your own')
+        
+        
+connect()
+
+def run_detection(frame):
+  print('run object detection')  
+
+#cap = cv2.VideoCapture("http://192.168.8.107:8080/stream/video.mjpeg")
 fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
 out = cv2.VideoWriter('output.mp4',fourcc, 10, (640,480))
 frameCount=0
-isStreaming = True
-while True:
-  ret, frame = cap.read()  
+
+while True:   
+  if isConnected == False:
+    continue  
+  ret, frame = stream1.read()  
   if ret == True:
-    # if frameCount%10 == 0:
-    #   frameCount = 0 
-    #   out.write(frame)       
-    # frameCount+=1    
-    cv2.imshow('Video', frame)  
-    #out.write(frame)   
+    if frameCount%10 == 0:
+      frameCount = 0
+      run_detection(frame) 
+      #out.write(frame)       
+    frameCount+=1    
+    cv2.imshow('Video', frame)     
     if cv2.waitKey(1) & 0xFF == ord('q'):
           break
   else:
-    print('broken pipe')
-    print(cap)
-    isStreaming = False
+    print('broken pipe, reconnect')
+    isConnected = False    
+    stream1.release()
+    out.release()
+    cv2.destroyAllWindows()
+    t=threading.Timer(5,connect)
+    t.start()
 print('cap broken')    
-cap.release()
+stream1.release()
 out.release()
 cv2.destroyAllWindows()
+
